8270a34741283ff76d3194d38bb8d1ad3818a34e
Holly Mccoy <holly.e.mccoy@gmail.com>
2021-03-27 13:42:10 -0400

    zipcode now found upon app opening-removed globals from manifest

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -4,16 +4,47 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.LocationServices;
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static String postalcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //GPS check permissions
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // request permission
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            } else {
+                //req location permission
+                startService();
+
+            }
+        } else {
+            // Start locations service
+            startService();
+        }
+
+        Log.d("mylog from main", String.valueOf(MainActivity.postalcode));
+
+        getIntent().getAction().equals("ACT_LOC");
+
+
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav_bar);
@@ -55,4 +86,54 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
     };
+
+    /*
+GPS functions
+ */
+
+    void startService(){
+
+        LocationBroadcastReciever reciever = new LocationBroadcastReciever();
+        IntentFilter filter = new IntentFilter("ACT_LOC");
+        registerReceiver(reciever,filter);
+        Intent intent = new Intent(MainActivity.this, GPS.class);
+
+        startService(intent);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startService();
+                }else{
+                    Toast.makeText(this, "Give me permissions", Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    public class LocationBroadcastReciever extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction().equals("ACT_LOC")) {
+                double latitude = intent.getDoubleExtra("latitude", 0f);
+                double longitude = intent.getDoubleExtra("longitude", 0f);
+                postalcode = intent.getStringExtra("postalcode");
+                Toast.makeText(MainActivity.this, "Lat: " + latitude + ", longi is: " + longitude + " postalcode: " + postalcode, Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+    }
+
+    public static String getPostalcode(){
+
+        return postalcode;
+    }
+
 }
+
