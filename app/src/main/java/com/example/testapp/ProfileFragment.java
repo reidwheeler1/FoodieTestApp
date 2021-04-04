c787cc51342f9e77ba98c6c410c31ac24d9f0804
Holly Mccoy <holly.e.mccoy@gmail.com>
2021-04-04 18:00:26 -0400

    added expandable list view (still not working though)

--- app/src/main/java/com/example/testapp/ProfileFragment.java
+++ app/src/main/java/com/example/testapp/ProfileFragment.java
@@ -2,27 +2,78 @@ package com.example.testapp;
 
 import android.media.Image;
 import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
+import android.widget.Adapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeableMethod;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class ProfileFragment extends Fragment {
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
+    ExpandableListView expandableListView;
+    ArrayList<String> listGroup = new ArrayList<>();
+    HashMap<String,ArrayList<String>> listChild = new HashMap<>();
+    profileAdapter adapter;
+
+    private RelativeLayout progressBar;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_profile, parent, false);
+        // Defines the xml file for the fragment // Inflate the layout for this fragment
+
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_quiz_card, parent, false);
+        progressBar = root.findViewById(R.id.loadingPanel);
+        init(root);
+        return root;
+
+
     }
 
-    // This event is triggered soon after onCreateView().
-    // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        // Setup any handles to view objects here
-        // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+    private void init(View root) {
+
+        ExpandableListView expandableListView = root.findViewById(R.id.expandableListView);
+
+        for (int g = 0 ; g<= 10; g++)
+        {
+            //add list values
+            listGroup.add("Group"+g);
+            //init array list
+            ArrayList<String> arrayList = new ArrayList<>();
+            //loop
+            for (int c= 0; c<=6; c++){
+                arrayList.add("item"+c);
+            }
+            listChild.put(listGroup.get(g),arrayList);
+        }
+
+        adapter = new profileAdapter(listGroup,listChild);
+        expandableListView.setAdapter(adapter);
     }
+
 }
